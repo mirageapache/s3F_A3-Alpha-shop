0c546c46afe3d1621f8e864235592804f050e1db
James <mirageapache@gmail.com>
2023-02-09 16:39:55 +0800

    Modify StepOne function

--- src/components/Main/Step/StepOne.js
+++ src/components/Main/Step/StepOne.js
@@ -1,5 +1,16 @@
 import style from '../../../styles/css/StepOne.module.css'
 
+const citys = [{name: '請選擇縣市', value: ''},{name: '基隆市', value: 'KLU'},{name: '新北市', value: 'TPH'},
+              {name: '臺北市', value: 'TPE'},{name: '桃園市', value: 'TYC'},{name: '新竹縣', value: 'HSH'},
+              {name: '新竹市', value: 'HSC'},{name: '苗栗市', value: 'MAC'},{name: '苗栗縣', value: 'MAL'},
+              {name: '臺中市', value: 'TXG'},{name: '彰化縣', value: 'CWH'},{name: '彰化市', value: 'CWS'},
+              {name: '南投市', value: 'NTC'},{name: '南投縣', value: 'NTO'},{name: '雲林縣', value: 'YLH'},
+              {name: '嘉義縣', value: 'CHY'},{name: '嘉義市', value: 'CYI'},{name: '臺南市', value: 'TNN'},
+              {name: '高雄市', value: 'KHH'},{name: '屏東縣', value: 'IUH'},{name: '屏東市', value: 'PTS'},
+              {name: '宜蘭縣', value: 'ILN'},{name: '宜蘭市', value: 'ILC'},{name: '花蓮縣', value: 'HWA'},
+              {name: '花蓮市', value: 'HWC'},{name: '臺東市', value: 'TTC'},{name: '臺東縣', value: 'TTT'},
+              {name: '澎湖縣', value: 'PEH'},{name: '金門縣', value: 'KMN'},{name: '連江縣', value: 'LNN'}]
+
 export default function StepOne(){
   return(
     <div id="step_one" className="container">
@@ -47,8 +58,7 @@ export default function StepOne(){
               <p>縣市</p>
             </label>
             <select id='city_input' name="city">
-              <option>請選擇縣市</option>
-              <option>台北市</option>
+              {citys.map(city => <option value={city.value}>{city.name}</option>)}
             </select>
           </span>
 
