89fa6e14e99591d0d6c2fa30313dbdbe0043c347
James <mirageapache@gmail.com>
2023-02-15 15:22:03 +0800

    Revise Cart product plus & minus function

--- src/components/Main/Cart.jsx
+++ src/components/Main/Cart.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react'
+
+// 載入css stylesheet
 import style from '../../styles/css/Cart.module.css'
 
 // 載入 SVG
@@ -5,7 +8,7 @@ import {ReactComponent as SvgIconPlus} from '../../sources/icons/plus.svg'
 import {ReactComponent as SvgIconMinus} from '../../sources/icons/minus.svg'
 
 // 資料宣告
-const datas = [
+const products = [
   {
     id: '1',
     name: '貓咪罐罐',
@@ -21,27 +24,89 @@ const datas = [
     quantity: 1
   },
 ]
-
-
-const infos = [{title:'運費', content:'免費'}, {title:'小計', content:'$5,298'}]
+const convey_info = {title:'運費', content:'免費'}
+const total_price = {title:'小計', content: 400}
 
 // 購物籃compontents
 export default function Cart(){
+  const [product , setProduct] = useState(products)
+  const [convey, setConvey] = useState(convey_info)
+  const [total, setTotal] = useState(total_price)
+
+  // 減少商品數量
+  function handleDecrease (id) {
+    let sum = 0 //總金額變數
+
+    // 設定quantity數量
+    setProduct( 
+      product.map(item => {
+        if(item.id === id){
+          if(item.quantity > 0){
+            let new_auqntity = item.quantity - 1 //暫存修改後的數量
+            sum += item.price * new_auqntity //加總總金額
+            // id相符 且 數量大於0才減少數量
+            return{ ...item, quantity: new_auqntity}
+          }
+          else{
+            sum += item.price * item.quantity
+            return{ ...item, quantity: 0 }
+          }
+        }
+        sum += item.price * item.quantity
+        return item
+      })
+    )
+
+    // 設定小計金額
+    setTotal({title: '小計', content: sum})
+  }
+
+  // 增加商品數量
+  function handleIncrease (id){
+    let sum = 0 //總金額變數
+
+    // 設定quantity數量
+    setProduct(
+      product.map(item => {
+        if(item.id === id){
+          let new_quantity = item.quantity + 1 //暫存修改後的數量
+          sum += item.price * new_quantity //加總總金額
+          // id相符則增加數量
+          return{ ...item, quantity: new_quantity }
+        }
+        else{
+          sum += item.price * item.quantity
+          return item
+        }
+      })
+    )
+
+    // 設定小計金額
+    setTotal({title: '小計', content: sum})
+  }
+
   return(
     <div id={style.cart_div} className="container">
       <h3 className={style.title}>購物籃</h3>
       <div className={style.item_div}>
-        {datas.map(data => <GoodsItem key={data.id} data={data}/>)}
+        {product.map(data => 
+          <GoodsItem 
+            key={data.id} 
+            data={data} 
+            onIncrease={handleIncrease} //將function 傳到子元素
+            onDecrease={handleDecrease} //將function 傳到子元素
+          />)}
       </div>
       <div className={style.cart_info}>
-        {infos.map((info, index) => <CartInfo key={index} info={info}/>)}
+        <CartInfo info={convey}/> {/* 運送方式 */}
+        <CartInfo info={total}/> {/* 小計 */}
       </div>
     </div>
   )
 }
 
 // 商品資訊 component
-function GoodsItem ({data}) {
+function GoodsItem ({data, onIncrease, onDecrease}) {
   return(
     <>
       <span className={style.item} key={data.id}>
@@ -49,16 +114,16 @@ function GoodsItem ({data}) {
         <span className={style.item_info}>
           <p className={style.item_title}>{data.name}</p>
           <span className={style.item_control}>
-            <a className={style.dec_btn} href="#">
+            <button className={style.dec_btn} onClick={() => {onDecrease(data.id)}}> {/*設定onDecrease()*/}
               <SvgIconMinus />  
-            </a>
+            </button>
             <p className={style.amount}>{data.quantity}</p>
-            <a className={style.add_btn} href="#">
+            <button className={style.add_btn} onClick={() => {onIncrease(data.id)}}> {/*設定onIncrease()*/}
               <SvgIconPlus />
-            </a>
+            </button>
           </span>
         </span>
-          <p className={style.item_price}>${data.price}</p>
+          <p className={style.item_price}>$ {data.price}</p>
       </span>
     </>
   )
@@ -70,7 +135,7 @@ function CartInfo ({info}) {
     <>
       <span className={style.info_item}>
         <p className={style.info_title}>{info.title}</p>
-        <p className={style.info_content}>{info.content}</p>
+        <p className={style.info_content}>{info.content > 0 && '$ '}{info.content}</p>
       </span>
     </>
   )
